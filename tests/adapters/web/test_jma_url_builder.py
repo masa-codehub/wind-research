fe6a92505478ecce96d764b483a75a1b7d6a6f08
masatake <mastake.home@outlook.jp>
2025-05-28 15:10:45 +0000

    Update resource files with new weather data and HTML templates
    - Replaced binary GIF files for day2, month2, and year2 with updated versions.
    - Added new HTML file for past weather data (sample_jma_a1_p51_b1638_20240101.html) containing detailed weather metrics.
    - Introduced a new HTML file (sample_jma_no_wind_dir.html) for testing purposes, which excludes wind direction data.

--- tests/adapters/web/test_jma_url_builder.py
+++ tests/adapters/web/test_jma_url_builder.py
@@ -4,6 +4,17 @@ from src.adapters.web.jma_url_builder import JmaUrlBuilder
 import pytest
 
 
+def test_build_url_for_prec51_block1638():
+    builder = JmaUrlBuilder()
+    point = ObservationPointValue(prefecture_no="51", block_no="1638")
+    target_date = date(2024, 1, 1)
+    actual_url = builder.build_jma_10min_data_url(point, target_date)
+    expected_url = (
+        "https://www.data.jma.go.jp/stats/etrn/view/10min_a1.php?prec_no=51&block_no=1638&year=2024&month=1&day=1&view=p1"
+    )
+    assert actual_url == expected_url
+
+
 def test_build_url_correctly():
     # Arrange
     builder = JmaUrlBuilder()
@@ -14,7 +25,7 @@ def test_build_url_correctly():
     actual_url = builder.build_jma_10min_data_url(point, target_date)
 
     # Assert
-    expected_url = "https://www.data.jma.go.jp/stats/etrn/view/10min_s1.php?prec_no=44&block_no=47662&year=2024&month=05&day=27&view=p1"
+    expected_url = "https://www.data.jma.go.jp/stats/etrn/view/10min_a1.php?prec_no=44&block_no=47662&year=2024&month=5&day=27&view=p1"
     assert actual_url == expected_url
 
 
@@ -23,5 +34,5 @@ def test_build_url_zero_padding():
     point = ObservationPointValue(prefecture_no="01", block_no="47401")
     target_date = date(2024, 1, 5)
     actual_url = builder.build_jma_10min_data_url(point, target_date)
-    expected_url = "https://www.data.jma.go.jp/stats/etrn/view/10min_s1.php?prec_no=01&block_no=47401&year=2024&month=01&day=05&view=p1"
+    expected_url = "https://www.data.jma.go.jp/stats/etrn/view/10min_a1.php?prec_no=01&block_no=47401&year=2024&month=1&day=5&view=p1"
     assert actual_url == expected_url
